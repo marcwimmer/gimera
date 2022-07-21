eadf5f2363c55247e74335ac93a02b28a6ee7d34
cicd <cicd>
2022-07-21 08:56:38 +0200

    [main]

--- gimera/tests/test_gimera.py
+++ gimera/tests/test_gimera.py
@@ -1035,5 +1035,55 @@ def test_clean_a_submodule_in_submodule(temppath):
     submodule_repo1 = main_repo.get_submodule("folder_of_repo1")
     submodule_repo1.force_remove_submodule('folder_of_repo2/repo2')
 
+def test_2_submodules(temppath):
+    workspace = temppath / "workspace_2submodules"
+    workspace.mkdir(exist_ok=True)
+    os.chdir(workspace.parent)
+
+    repo_main = _make_remote_repo(workspace / "mainrepo")
+    repo_1 = _make_remote_repo(workspace / "repo1")
+    repo_2 = _make_remote_repo(workspace / "repo2")
+
+    # make variant repo and change on a branch there
+    with clone_and_commit(repo_1, "main") as repopath:
+        (repopath / "repo1.txt").write_text("This is a new function")
+        Repo(repopath).simple_commit_all()
+
+    with clone_and_commit(repo_2, "main") as repopath:
+        (repopath / "repo2.txt").write_text("This is a new function")
+        Repo(repopath).simple_commit_all()
+
+    repos = {
+        "repos": [
+            {
+                "url": f"file://{repo_1}",
+                "branch": "main",
+                "path": "repo1",
+                "type": "submodule",
+            },
+            {
+                "url": f"file://{repo_2}",
+                "branch": "main",
+                "path": "repo2",
+                "type": "submodule",
+            },
+        ]
+    }
+
+    workspace_main = workspace / "main_working"
+    main_repo = Repo(workspace_main)
+    subprocess.check_call(["git", "clone", f"file://{repo_main}", workspace_main])
+    (workspace_main / 'gimera.yml').write_text(yaml.dump(repos))
+
+    os.chdir(workspace_main)
+    gimera_apply([], None)
+
+    assert (workspace_main / "repo1" / "repo1.txt").exists()
+    assert (workspace_main / "repo2" / "repo2.txt").exists()
+
+    submodule_repo1 = main_repo.get_submodule("repo1")
+    submodule_repo2 = main_repo.get_submodule("repo2")
+    assert submodule_repo1
+    assert submodule_repo2
 
 # test change something in submodule and check if gets lost if not pushed
