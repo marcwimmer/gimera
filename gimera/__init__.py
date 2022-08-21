934cc3b223bc90465eed3dd145dc57e257476a33
Marc Wimmer <marc@itewimmer.de>
2022-08-21 22:26:05 +0200

    [main] include tests

--- gimera/__init__.py
+++ gimera/__init__.py
@@ -4,4 +4,10 @@ from . import gimera
 from . import repo
 from . import gitcommands
 from . import tools
-from . import tests
+
+try:
+    from . import tests
+except ModuleNotFoundError:
+    pass
+except ImportError:
+    pass
