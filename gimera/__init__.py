894c74d43c3670a3bcf169930d8372d9d8c1ef02
Marc Wimmer <marc@itewimmer.de>
2022-08-22 09:14:06 +0200

    removed import statement

--- gimera/__init__.py
+++ gimera/__init__.py
@@ -4,10 +4,3 @@ from . import gimera
 from . import repo
 from . import gitcommands
 from . import tools
-
-try:
-    from . import tests
-except ModuleNotFoundError:
-    pass
-except ImportError:
-    pass
