5ed9ca49b0ab5f1ab0e2113bd24b05ba05c39ee6
Marc Wimmer <marc@itewimmer.de>
2023-07-17 12:15:23 +0200

    [main]

--- gimera/consts.py
+++ gimera/consts.py
@@ -1,6 +1,9 @@
 import inquirer
 gitcmd = ["git", "-c", "protocol.file.allow=always"]
-inquirer_theme = inquirer.themes.GreenPassion()
+try:
+    inquirer_theme = inquirer.themes.GreenPassion()
+except:
+    inquirer_theme = None
 
 REPO_TYPE_INT = "integrated"
 REPO_TYPE_SUB = "submodule"
